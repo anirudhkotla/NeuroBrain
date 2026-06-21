977bd1bbb11926bcfa76ba7740ba90661b0a8927
anirudhkotla <anirudh@tericsoft.com>
2026-06-21 10:54:48 +0530

    dummy(anomalous): deeply nested high-complexity

---
+++ scrutinizer_tests/anomalous_1782019484.py
@@ -0,0 +1,42 @@
+def tangle(a):
+    if a > 0:
+        for i in range(0):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    if a > 1:
+        for i in range(1):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    if a > 2:
+        for i in range(2):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    if a > 3:
+        for i in range(3):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    if a > 4:
+        for i in range(4):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    if a > 5:
+        for i in range(5):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    if a > 6:
+        for i in range(6):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    if a > 7:
+        for i in range(7):
+            while i > 0:
+                if i % 2 == 0:
+                    i -= 1
+    return a
